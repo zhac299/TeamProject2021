e041866f7143045176cc07869ca73ef412fcf95a
David Akinsanya <davidakinsanya00@gmail.com>
2021-01-23 16:31:33 +0000

    beginning to set up routing between login and waiter menu components

--- RestaurantWebApp/src/app/app-routing.module.ts
+++ RestaurantWebApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InputComponent } from './login-input/login-input.component';
+import { WaiterMenuComponent } from './waiter-menu/waiter-menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: InputComponent },
+    {path: 'waiter-menu', component: WaiterMenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
