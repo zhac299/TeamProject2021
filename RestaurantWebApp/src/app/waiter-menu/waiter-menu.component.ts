3a1c845d8ff073a60f2ce8cb458e0e430c994979
saad-imran98 <saad2379@gmail.com>
2021-01-23 17:51:32 +0000

    Waiter menu shows dynamic html based on ordered/notOrdered tables

--- RestaurantWebApp/src/app/waiter-menu/waiter-menu.component.ts
+++ RestaurantWebApp/src/app/waiter-menu/waiter-menu.component.ts
@@ -9,14 +9,18 @@ import {Table} from '../../models/Table';
 })
 export class WaiterMenuComponent implements OnInit {
 
-
-
   constructor(private tableService: TableService) { }
 
   tableList: Table[] = [];
+  freeTables = 0;
 
   ngOnInit(): void {
     this.tableService.getTables().subscribe( orders => {
+      // checks which incoming tables have orders
+      for (const table of orders){
+        // updates counter
+        if (table.hasOrder){this.freeTables++; }
+      }
       this.tableList = orders;
     });
   }
