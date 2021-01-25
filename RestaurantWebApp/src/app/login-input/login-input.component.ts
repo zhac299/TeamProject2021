d737c8b5be756bcc3ed0ae1b6cdd8a6c37ca8cf3
David Akinsanya <davidakinsanya00@gmail.com>
2021-01-25 00:42:58 +0000

    program edits.

--- RestaurantWebApp/src/app/login-input/login-input.component.ts
+++ RestaurantWebApp/src/app/login-input/login-input.component.ts
@@ -32,7 +32,6 @@ export class LoginInputComponent implements OnInit {
             kitchen: this.kitchen
         }
 
-        console.log(login2.waiter);
         this.input.getLogin().subscribe(login => {
 
             this.loginTwo = login;        
