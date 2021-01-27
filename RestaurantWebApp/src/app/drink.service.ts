e98dc47104c871dc44e9d6ee0817397d9dc50cd2
saad-imran98 <saad2379@gmail.com>
2021-01-27 20:50:40 +0000

    Button sizing fix

--- RestaurantWebApp/src/app/drink.service.ts
+++ RestaurantWebApp/src/app/drink.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Order} from "../models/Order";
 import {map} from "rxjs/operators";
 import {Drink} from "../models/Drink";
 
