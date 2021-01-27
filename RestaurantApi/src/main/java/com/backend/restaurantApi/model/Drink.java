86f29d7e7e097fb361988d62f344b0af7497e19b
Laadi Hamdou <lhamdouuni@gmail.com>
2021-01-27 20:29:53 +0000

    Added the Drink model, repo and controller.

---
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Drink.java
@@ -0,0 +1,30 @@
+package com.backend.restaurantApi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class Drink {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "drinkName")
+    private String drinkName;
+
+    // used to serialize object to json
+    @Override
+    public String toString() {
+        return "Main{" +
+                "id=" + id +
+                ", drinkName='" + drinkName + '\'' +
+                '}';
+    }
+}
---
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/repository/DrinkRepository.java
@@ -0,0 +1,9 @@
+package com.backend.restaurantApi.repository;
+
+
+import com.backend.restaurantApi.model.Drink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DrinkRepository extends JpaRepository<Drink, Long> {}
