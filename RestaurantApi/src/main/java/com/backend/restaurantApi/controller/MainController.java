9e97102e30a8c3353ddcd7d1ae2f990294213fa2
Laadi Hamdou <lhamdouuni@gmail.com>
2021-01-27 23:37:22 +0000

    Added basic PostMapping, DeleteMapping and PutMapping methods in MainController where logic will go to allow manipulation of data within the database from the front end.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/MainController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/MainController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Console;
 import java.util.List;
@@ -29,5 +26,23 @@ public class MainController {
     @GetMapping
     public List<Main> index() {
         return mainRepo.findAll();
+    }
+
+    //Adds mains
+    @PostMapping
+    public String addMain() {
+        return"HTTP POST request recieved";
+    }
+
+    //Deletes mains
+    @DeleteMapping
+    public String deleteMain() {
+        return"HTTP DELETE request recieved";
+    }
+
+    //Updates mains
+    @PutMapping
+    public String updateMain() {
+        return"HTTP PUT request recieved";
     }
 }
