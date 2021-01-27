49afa79e8407c8daef67ccba96ae6dba4f88e40e
saad-imran98 <saad2379@gmail.com>
2021-01-27 00:34:28 +0000

    Fixed duplicate import

--- RestaurantWebApp/src/app/app-routing.module.ts
+++ RestaurantWebApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Routes, RouterModule } from '@angular/router';
 import { CustomerInterfaceComponent } from './customer-interface/customer-interface.component';
 import { InitialMenuComponent } from './initial-menu/initial-menu.component';
 import { LoginInputComponent } from './login-input/login-input.component';
@@ -8,7 +7,6 @@ import { OrderListComponent } from './order-list/order-list.component';
 import { WaiterMenuComponent } from './waiter-menu/waiter-menu.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {OrderComponent} from './waiter-menu/order/order.component';
-
 
 const routes: Routes = [
   {path: 'table/:id', component: OrderComponent},
