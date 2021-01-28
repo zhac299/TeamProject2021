6d572eb3ed07c8e98c739ac5847f623869d287d4
Laadi Hamdou <lhamdouuni@gmail.com>
2021-01-28 12:57:28 +0000

    Added basic PostMapping, DeleteMapping and PutMapping methods in StarterController where logic will go to allow manipulation of data within the database from the front end.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/StarterController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/StarterController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Console;
 import java.util.List;
@@ -29,5 +26,23 @@ public class StarterController {
     @GetMapping
     public List<Starter> index() {
         return starterRepo.findAll();
+    }
+
+    //Adds mains
+    @PostMapping
+    public String addStarter() {
+        return"HTTP POST request recieved";
+    }
+
+    //Deletes mains
+    @DeleteMapping
+    public String deleteStarter() {
+        return"HTTP DELETE request recieved";
+    }
+
+    //Updates mains
+    @PutMapping
+    public String updateStarter() {
+        return"HTTP PUT request recieved";
     }
 }
