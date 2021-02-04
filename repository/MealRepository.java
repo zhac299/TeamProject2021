e27382b6b35fddd5e1b0aa3c0fb89b935b1bb518
saad-imran98 <saad2379@gmail.com>
2021-02-04 23:06:23 +0000

    Revert "Add files via upload"
    This reverts commit dcf8245091971dbe79d79178421ed8e1035907d4.

--- repository/MealRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.restaurantApi.repository;
-
-import com.backend.restaurantApi.model.Meal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MealRepository extends JpaRepository<Meal, Long> {}
