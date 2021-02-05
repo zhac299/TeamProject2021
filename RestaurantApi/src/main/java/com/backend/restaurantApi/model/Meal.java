8ddb9cd8bd08617bdf180b4b2634c3f9647c1b42
saad-imran98 <saad2379@gmail.com>
2021-02-05 17:01:24 +0000

    Removed dead import

--- RestaurantApi/src/main/java/com/backend/restaurantApi/model/Meal.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Meal.java
@@ -1,7 +1,6 @@
 package com.backend.restaurantApi.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
