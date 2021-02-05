e8d34ba962a34ca6dcaa6e673441abcc145047f7
saad-imran98 <saad2379@gmail.com>
2021-02-05 16:17:20 +0000

    Order post request added

--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/OrderController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/OrderController.java
@@ -1,26 +1,30 @@
 package com.backend.restaurantApi.controller;
 
+import com.backend.restaurantApi.model.Meal;
 import com.backend.restaurantApi.model.Order;
 import com.backend.restaurantApi.repository.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping(path = "/api/v1/order")
+@RequestMapping(path = "/api/v1/")
 public class OrderController {
 
     @Autowired
     OrderRepository orderRepo;
 
-    @GetMapping
+    @GetMapping("/orders")
     public List<Order> index() {
       return orderRepo.findAll();
-  } 
+  }
+
+
+    @PostMapping("/orders")
+    public Order newOrder(@RequestBody Order order){
+        return orderRepo.save(order);
+    }
 }
