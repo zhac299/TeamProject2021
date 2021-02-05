f4ada42bb62ed567dfe07f3177e2936fa3372192
saad-imran98 <saad2379@gmail.com>
2021-02-05 11:06:02 +0000

    Revert "Add files via upload"
    This reverts commit 9702180e81504ba62f1bde3b54a3bf4f2e07b8a5.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/model/Meal.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Meal.java
@@ -1,15 +1,21 @@
 package com.backend.restaurantApi.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "meal")
+@SecondaryTable(name = "allergens", pkJoinColumns = @PrimaryKeyJoinColumn(name = "meal_id"))
 public class Meal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    Long id;;
+    Long id;
 
     @Column(name = "name")
     String name;
@@ -17,12 +23,12 @@ public class Meal {
     @Column(name = "price")
     Double price;
     
-    @OneToOne(mappedBy = "meal")
-    Allergens allergens;
+    @Embedded
+    Allergens allergens = new Allergens();
     
-    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "order_id", nullable = true)
-    Order order;
+//    @ManyToOne(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "order_id", nullable = true)
+//    Order order;
 
     @Override
     public String toString() {
@@ -31,39 +37,89 @@ public class Meal {
                 ", name='" + name + '\'' +
                 ", price='" + price + '\'' +
                 ", allergens='" + allergens + '\'' +
-                ", order='" + order + '\'' +
+//                ", order='" + order + '\'' +
                 '}';
     }
 
     public Meal() {}
 
-    public Meal(String name, double price, Allergens allergens) {
-       this.name = name;
-       this.price = price;
-       this.allergens = allergens;
-    }
+//    public Meal(String name, double price, Allergens allergens) {
+//       this.name = name;
+//       this.price = price;
+//       this.allergens = allergens;
+//    }
 
-    public void setOrder(Order order) {
-        this.order = order;
-    }
+//    public void setOrder(Order order) {
+//        this.order = order;
+//    }
+//
+//    public String getDishName() {
+//        return this.name;
+//    }
+//
+//    public long getId() {
+//        return this.id;
+//    }
+//
+//    public double getPrice() {
+//        return this.price;
+//    }
+//
+//    public Allergens getAllergens() {
+//        return this.allergens;
+//    }
 
-    public String getDishName() {
-        return this.name;
-    }
+//    public Order getOrder() {
+//        return this.order;
+//    }
+}
 
-    public long getId() {
-        return this.id;
-    }
+@Embeddable
+class Allergens {
 
-    public double getPrice() {
-        return this.price;
-    }   
+   @Column(name = "peanuts", table = "allergens")
+   boolean peanuts = false;
 
-    public Allergens getAllergens() {
-        return this.allergens;
-    }
+   @Column(name = "celery", table = "allergens")
+   boolean celery = false;
 
-    public Order getOrder() {
-        return this.order;
-    }
+   @Column(name = "gluten", table = "allergens")
+   boolean gluten = false;
+
+   @Column(name = "crustaceans", table = "allergens")
+   boolean crustaceans = false;
+
+   @Column(name = "eggs", table = "allergens")
+   boolean eggs = false;
+
+   @Column(name = "fish", table = "allergens")
+   boolean fish = false;
+
+   @Column(name = "lupin", table = "allergens")
+   boolean lupin = false;
+
+   @Column(name = "milk", table = "allergens")
+   boolean milk = false;
+
+   @Column(name = "molluscs", table = "allergens")
+   boolean molluscs = false;
+
+   @Column(name = "mustard", table = "allergens")
+   boolean mustard = false;
+
+   @Column(name = "nuts", table = "allergens")
+   boolean nuts = false;
+
+   @Column(name = "soya", table = "allergens")
+   boolean soya = false;
+
+   @Column(name = "sesame_seeds", table = "allergens")
+   boolean sesameSeeds = false;
+
+   @Column(name = "sulphites", table = "allergens")
+   boolean sulphites = false;
+
+
+   public Allergens() {}
+
 }
--- RestaurantApi/src/main/java/com/backend/restaurantApi/repository/AllergensRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.restaurantApi.repository;
-
-import com.backend.restaurantApi.model.Allergens;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AllergensRepository extends JpaRepository<Allergens, Long> {}
