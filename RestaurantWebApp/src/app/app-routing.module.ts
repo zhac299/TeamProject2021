7162494e98ee51d6402af05502cb39132fa1a8a9
Cosmin Sirbu <zhac254@live.rhul.ac.uk>
2021-02-07 23:02:59 +0200

    fixed an issue in the login component tester file (the import name was wrong) and fixed an issue in the app routing component: the path for the customer menu was routing to the home page instead to the the CustomerInterfaceComponent.

--- RestaurantWebApp/src/app/app-routing.module.ts
+++ RestaurantWebApp/src/app/app-routing.module.ts
@@ -9,13 +9,13 @@ import { HomePageComponent } from './home-page/home-page.component';
 import {OrderComponent} from './waiter-menu/order/order.component';
 
 const routes: Routes = [
-  {path: 'table/:id', component: OrderComponent},
-  { path: '', component: HomePageComponent },
+  { path: 'table/:id', component: OrderComponent},
+  { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginInputComponent },
   { path: 'waiter-menu', component: WaiterMenuComponent },
   { path: 'order-list', component: OrderListComponent },
   { path: 'initial-menu', component: InitialMenuComponent },
-  { path: 'home', component: CustomerInterfaceComponent }
+  { path: 'customer-menu', component: CustomerInterfaceComponent }
 ];
 
 @NgModule({
