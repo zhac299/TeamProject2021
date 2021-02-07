9fd8acca64d5dd6fb64eb7c2605df2cc182ec286
David Akinsanya <davidakinsanya00@gmail.com>
2021-02-07 11:40:09 +0000

    This commit is to ensure new files are tracked in git, VSCode had stated otherwise.

---
+++ RestaurantWebApp/src/app/menu-filter/filter.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Order } from 'src/models/Order';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+    mockDbUrl = 'http://localhost:3000/Orders'
+    constructor(private httpClient: HttpClient) { }
+    
+    getOrders(): Observable<Order[]> { 
+        return this.httpClient.get<Order[]>(this.mockDbUrl)
+        .pipe(
+          map(response => response)
+        );
+    }
+}
