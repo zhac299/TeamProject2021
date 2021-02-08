6e3beedb6feafa90d28f05a7da568d633320cded
saad-imran98 <saad2379@gmail.com>
2021-02-08 21:26:20 +0000

    Added addMenuMeal function

--- RestaurantApi/src/main/java/com/backend/restaurantApi/service/MenuService.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/service/MenuService.java
@@ -1,9 +1,14 @@
 package com.backend.restaurantApi.service;
 
+import com.backend.restaurantApi.exception.MealNotFoundException;
+import com.backend.restaurantApi.exception.MenuNotFoundException;
+import com.backend.restaurantApi.model.Meal;
 import com.backend.restaurantApi.model.Menu;
 import com.backend.restaurantApi.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MenuService {
@@ -11,7 +16,21 @@ public class MenuService {
     @Autowired
     MenuRepository menuRepository;
 
+    @Autowired
+    MealService mealService;
+
     public Menu createNewMenu(Menu menu) {
         return menuRepository.save(menu);
+    }
+
+    public Menu addMenuMeal(Meal meal) {
+        mealService.createNewMeal(meal);
+        Optional<Menu> menu = menuRepository.findById(meal.getId());
+        if(!menu.isPresent()){
+            throw new MenuNotFoundException("Menu record is not available...");
+        } else {
+            menu.get().getMeal().add(meal);
+        }
+        return menuRepository.save(menu.get());
     }
 }
