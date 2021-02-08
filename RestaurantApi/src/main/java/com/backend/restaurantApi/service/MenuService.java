cd9e4028dd05fbbc94f7b5cbfb817883a91fc4ef
saad-imran98 <saad2379@gmail.com>
2021-02-08 20:05:20 +0000

    Created Post request logic

--- RestaurantApi/src/main/java/com/backend/restaurantApi/service/MenuService.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/service/MenuService.java
@@ -1,4 +1,17 @@
 package com.backend.restaurantApi.service;
 
+import com.backend.restaurantApi.model.Menu;
+import com.backend.restaurantApi.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MenuService {
+
+    @Autowired
+    MenuRepository menuRepository;
+
+    public Menu createNewMenu(Menu menu) {
+        return menuRepository.save(menu);
+    }
 }
