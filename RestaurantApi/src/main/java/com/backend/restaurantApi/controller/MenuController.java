7428dcb7aa01c3bf8b30d5a6776446ba3a80fa04
saad-imran98 <saad2379@gmail.com>
2021-02-08 20:12:53 +0000

    RestController annotation bug fix

--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/MenuController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/MenuController.java
@@ -4,14 +4,14 @@ import com.backend.restaurantApi.model.Menu;
 import com.backend.restaurantApi.repository.MenuRepository;
 import com.backend.restaurantApi.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/v1/")
 public class MenuController {
 
