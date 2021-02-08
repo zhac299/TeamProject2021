1d00af15ee177dd6fba3e7b9e77dbbfc6f9ad706
saad-imran98 <saad2379@gmail.com>
2021-02-08 20:01:41 +0000

    Added post and get mappings

---
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/MenuController.java
@@ -0,0 +1,33 @@
+package com.backend.restaurantApi.controller;
+
+import com.backend.restaurantApi.model.Menu;
+import com.backend.restaurantApi.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/api/v1/")
+public class MenuController {
+
+    @Autowired
+    MenuRepository menuRepository;
+
+    @Autowired
+    MenuService menuService;
+
+    @GetMapping(path = "/menu")
+    public List<Menu> getMenu(){
+        return menuRepository.findAll();
+    }
+
+    @PostMapping(path = "/menu")
+    public Menu  createNewMenu(Menu menu){
+
+        return menuService.createNewMenu(menu);
+    }
+}
