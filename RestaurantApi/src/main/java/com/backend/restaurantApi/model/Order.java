6353e37df87e71d890b9d0f32585ba69fce64bfa
Hassan Mirza <zhac299@live.rhul.ac.uk>
2021-02-08 20:36:32 +0000

    Added CRUD Mapping for Order model.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/model/Order.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Order.java
@@ -45,7 +45,7 @@ public class Order {
         return id;
     }
 
-    public void setId(long id) {
+    public void setOrderId(Long id) {
         this.id = id;
     }
 
---
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.backend.restaurantApi.service;
+
+import java.util.Optional;
+
+import com.backend.restaurantApi.exception.OrderNotFoundException;
+import com.backend.restaurantApi.model.Order;
+import com.backend.restaurantApi.repository.OrderRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public Order createNewOrder(Order Order) {
+        return orderRepository.save(Order);
+    }
+
+	public Order getOrderById(Long OrderId) {
+		Optional<Order> optionalOrder = orderRepository.findById(OrderId);
+
+        if (!optionalOrder.isPresent()) {
+            throw new OrderNotFoundException("Order Record is not available...");
+        }
+        return optionalOrder.get();
+	}
+
+	public Order updateOrder(Long id, Order Order) {
+		Order.setOrderId(id);
+        return orderRepository.save(Order);
+	}
+
+	public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+	}
+    
+}
