b0d6bd5572dce77cc2be38033e63a7157fc179dc
saad-imran98 <saad2379@gmail.com>
2021-02-08 19:49:38 +0000

    Created Menu model to house the set food menu for the restaurant

---
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Menu.java
@@ -0,0 +1,26 @@
+package com.backend.restaurantApi.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurant_menu")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @JsonManagedReference
+    @Column(name = "meal", nullable = false)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "menu")
+    private List<Meal> meal;
+
+}
