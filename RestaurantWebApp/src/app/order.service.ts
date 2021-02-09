a14365cc9ba09dd2b62387f77827201829f5e2cd
saad-imran98 <saad2379@gmail.com>
2021-02-09 13:06:43 +0000

    Updated service to use Spring backend Orders

--- RestaurantWebApp/src/app/order.service.ts
+++ RestaurantWebApp/src/app/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 import {Order} from '../models/Order';
 import {map} from 'rxjs/operators';
 
@@ -10,14 +10,19 @@ import {map} from 'rxjs/operators';
 export class OrderService {
 
   mockDbUrl = 'http://localhost:3000/orders';
+  restaurantWebApiUrl = 'http://localhost:8080/api/v1/orders/';
 
   constructor(private httpClient: HttpClient) { }
 
   // Making an Http request to the database
   public getOrders(): Observable<Order[]> {
-    return this.httpClient.get<Order[]>(this.mockDbUrl)
+    return this.httpClient.get<Order[]>(this.restaurantWebApiUrl)
     .pipe(
       map(response => response)
     );
   }
+
+  // public createOrder(): Observable<ArrayBuffer> {
+  //   return this.httpClient.post(this.mockDbUrl);
+  // }
 }
