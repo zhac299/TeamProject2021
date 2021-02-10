d65e1a81e4867e7d78edf4457f854453d5c6d8ff
saad-imran98 <saad2379@gmail.com>
2021-02-10 19:05:28 +0000

    Added menu items from menu service

--- RestaurantWebApp/src/app/customer-interface/customer-interface.component.ts
+++ RestaurantWebApp/src/app/customer-interface/customer-interface.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {OrderService} from '../order.service';
 import {Order} from '../../models/Order';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
+import {MenuService} from "../menu.service";
+import {Menu} from "../../models/Menu";
 
 interface Food {
   viewValue: string;
@@ -25,17 +27,20 @@ export class CustomerInterfaceComponent implements OnInit {
     {viewValue: 'Dips', mappedOrders:[this.orderList[1]], selected: false},
     {viewValue: 'Deserts', mappedOrders:[this.orderList[1]], selected: false}
   ];
-  
-  constructor(private orderService: OrderService) { }
+  menuList: Menu[];
+
+  constructor(private orderService: OrderService,
+              private menuService: MenuService) { }
 
   ngOnInit(): void {
     this.orderService.getOrders().subscribe( orders => {
       this.orderList = orders;
     });
+    this.menuService.getMenu().subscribe(menuItems => this.menuList = menuItems);
   }
 
   findCategory(food): void {
-    for(let f of this.foods) { 
+    for(let f of this.foods) {
       if (food == f) {
         f.selected = true;
       } else {
