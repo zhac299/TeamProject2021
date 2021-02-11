fca8128a16d41fc8d1c92a45b1d7b24d4c1fc6fa
saad-imran98 <saad2379@gmail.com>
2021-02-11 17:10:07 +0000

    Dialog componenet for adding new order added.

---
+++ RestaurantWebApp/src/app/waiter-menu/add-order-dialog/add-order-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-order-dialog',
+  templateUrl: './add-order-dialog.component.html',
+  styleUrls: ['./add-order-dialog.component.sass']
+})
+export class AddOrderDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
