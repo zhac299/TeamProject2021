fe80e3c7d851275beecd9f766b3c3e788bd3acf1
Cosmin Sirbu <zhac254@live.rhul.ac.uk>
2021-02-17 13:10:23 +0200

    Added a tables filed on which the mat-option loops with an ng-for to display all the tables

--- RestaurantWebApp/src/app/customer-interface/call-waiter/call-waiter-dialog/call-waiter-dialog.component.ts
+++ RestaurantWebApp/src/app/customer-interface/call-waiter/call-waiter-dialog/call-waiter-dialog.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CallWaiterDialogComponent implements OnInit {
 
   constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  tables: string[] = ["Table1", "Table2", "Table3", "Table4", "Table 5", "Table 6", "Table 7"];
 }
