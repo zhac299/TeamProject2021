1118bf2c8fde0e89b5bf4d17493e930f81e6341a
Laadi Hamdou <lhamdouuni@gmail.com>
2021-02-17 19:26:34 +0000

    Added query to the staff repository to query the database and check if the username and password provided exists.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/repository/StaffRepository.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/repository/StaffRepository.java
@@ -3,8 +3,16 @@ package com.backend.restaurantApi.repository;
 
 import com.backend.restaurantApi.model.Staff;
 
+import  java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StaffRepository extends JpaRepository<Staff, Long> {}
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+    @Query
+    ("SELECT * from staff  WHERE user_name = ?1 AND password = ?2")
+    List<Staff> getStaffMember(@Param("user_name") String userName, @Param("password")String password);
+
+}
