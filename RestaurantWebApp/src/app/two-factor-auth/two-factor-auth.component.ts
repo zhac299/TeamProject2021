b3973ea0213c10867430b2417a8ade090e8d272f
David Akinsanya <davidakinsanya00@gmail.com>
2021-02-18 22:08:22 +0000

    Adding a new component to angular so we can commence the implementation of two-factor authentication.

---
+++ RestaurantWebApp/src/app/two-factor-auth/two-factor-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-factor-auth',
+  templateUrl: './two-factor-auth.component.html',
+  styleUrls: ['./two-factor-auth.component.sass']
+})
+export class TwoFactorAuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
