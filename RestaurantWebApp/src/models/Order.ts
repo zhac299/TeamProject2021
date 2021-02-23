93da6eba8424e980992b62a4a7c38c946343268b
saad-imran98 <saad2379@gmail.com>
2021-02-23 20:35:01 +0000

    Updated Models to reflect backend changes

--- RestaurantWebApp/src/models/Order.ts
+++ RestaurantWebApp/src/models/Order.ts
@@ -1,11 +1,14 @@
 import {Meal} from './Meal';
+import {Table} from './Table';
+import {Customer} from "./Customer";
 
 export class Order {
   id: number;
   meal: Meal[];
-  category: String
+  category: string;
   isDelivered: boolean;
-  customerTableNum: number;
+  table: Table;
   waiterId: number;
   date: Date;
+  customer: Customer;
 }
