56f5e3e7310f343df648aab06be33033dc1ecbf0
Laadi Hamdou <lhamdouuni@gmail.com>
2021-02-26 02:02:33 +0000

    Added missing autowired import in email service.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/service/EmailService.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/service/EmailService.java
@@ -1,5 +1,6 @@
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmailService{
 
