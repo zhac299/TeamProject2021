dc064cdd4f9c80733f544d228aa35ad59e2b0980
Hassan Mirza <zhac299@live.rhul.ac.uk>
2021-03-10 08:45:52 +0000

    Commented out project breaking code made by yuvi - needs fixing.

--- RestaurantWebApp/src/app/orders-list-display/orders-list-display.component.ts
+++ RestaurantWebApp/src/app/orders-list-display/orders-list-display.component.ts
@@ -83,6 +83,7 @@ export class OrdersListDisplayComponent implements OnInit, OnDestroy {
       this.orderService.createNewOrderWithCustomer(a)
     );
   }
+  
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
