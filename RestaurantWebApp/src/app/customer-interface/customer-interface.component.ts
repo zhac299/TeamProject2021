37b174603a6b01a1d1e56c1582596dc0a8c15c7a
saad-imran98 <saad2379@gmail.com>
2021-03-13 21:51:26 +0000

    Made animations faster

--- RestaurantWebApp/src/app/customer-interface/customer-interface.component.ts
+++ RestaurantWebApp/src/app/customer-interface/customer-interface.component.ts
@@ -31,10 +31,10 @@ import { coerceStringArray } from '@angular/cdk/coercion';
       transition('*=>*', [
         query(':enter', style({opacity: 0}), {optional: true}),
 
-        query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
+        query(':enter', stagger('100ms', [
+          animate('.5s ease-in', keyframes([
             style({opacity: 0, transform: 'translateY(-50px)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: .5, transform: 'translateY(15px)', offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1})
           ]))
         ]))
@@ -81,7 +81,7 @@ export class CustomerInterfaceComponent implements OnInit {
       this.categories = categories;
       this.selectedCategory = this.categories[0];
     });
-    
+
     this.findCategoryItems();
   }
 
