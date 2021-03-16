0c5891aa58f73617bebec2117ef331428e7b1755
saad-imran98 <saad2379@gmail.com>
2021-03-16 22:35:02 +0000

    Added background image and title

--- RestaurantWebApp/src/app/landing-page/landing-page.component.ts
+++ RestaurantWebApp/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewEncapsulation} from '@angular/core';
+import {fromEvent} from "rxjs";
+import {debounceTime, map, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.sass']
+  styleUrls: ['./landing-page.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, AfterViewInit {
+  windowWidth: number;
+  windowHeight: number;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
+  resize$ = fromEvent(window, 'resize');
   ngOnInit(): void {
+    this.resize$
+      .pipe(debounceTime(50),
+        tap(evt=>console.log('window.innerWidth=', window.innerWidth, this.windowWidth)),
+        map((w) => {
+          return window
+        })
+      )
+      .subscribe((w) => {
+        this.windowWidth = w.innerWidth;
+        this.windowHeight = w.innerHeight;
+        });
+  }
+
+  ngAfterViewInit(): void {
+    this.elementRef.nativeElement.ownerDocument.body.backgroundImage = 'url(assets/images/background.jpg)';
+    this.elementRef.nativeElement.ownerDocument.body.width = this.windowWidth;
   }
 
 }
