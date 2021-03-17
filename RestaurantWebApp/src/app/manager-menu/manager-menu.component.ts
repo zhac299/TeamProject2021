0465bf233994332adc5a793a9f7ecf6b0f45fcd7
Cosmin Sirbu <zhac254@live.rhul.ac.uk>
2021-03-17 18:24:18 +0200

    binded the buttons to the functions

--- RestaurantWebApp/src/app/manager-menu/manager-menu.component.ts
+++ RestaurantWebApp/src/app/manager-menu/manager-menu.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { MatListModule} from '@angular/material/list';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Menu } from 'src/models/Menu';
+import { Table } from 'src/models/Table';
+import { CustomerService } from '../customer.service';
+import { MenuService } from '../menu.service';
 import { MenusListDisplayComponent } from '../menus-list-display/menus-list-display.component';
+import { OrderService } from '../order.service';
+import { TableService } from '../table.service';
+import { AddMenuDialogComponent } from '../waiter-menu/add-menu-dialog/add-menu-dialog.component';
+import { PickTableDialogComponent } from '../waiter-menu/pick-table-dialog/pick-table-dialog.component';
 @Component({
   selector: 'app-manager-menu',
   templateUrl: './manager-menu.component.html',
@@ -8,9 +18,49 @@ import { MenusListDisplayComponent } from '../menus-list-display/menus-list-disp
 })
 export class ManagerMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private orderService: OrderService,
+    private customerService: CustomerService,
+    public dialog: MatDialog,
+    private tableService: TableService,
+    private menuService: MenuService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  openSelectTableDialog(): Observable<Table> {
+    const dialogRef = this.dialog.open(PickTableDialogComponent);
+    return dialogRef.afterClosed();
+  }
+
+  createNewOrder(): void {
+    this.openSelectTableDialog()
+      .pipe(
+        switchMap((dialogResult) =>
+          this.customerService.createCustomerWithTable(dialogResult))
+      ).subscribe((a) =>
+        this.orderService.createNewOrderWithCustomer(a)
+      );
+  }
+
+  createNewTable(): void {
+    this.tableService.createTable().subscribe();
+  }
+
+  openAddMenuDialog() {
+    const title = "Add New Dish";
+    let menu: Menu = new Menu();
+    const dialogRef = this.dialog.open(AddMenuDialogComponent, {
+      data: {menu,title},
+      width: '50%',
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(menu => {
+      if(menu){
+        this.menuService.createMenuItem(menu);
+      }
+    })
   }
 
 }
