76aa2790dc20bf812d702e6bd592d5b39a2a1f9b
saad-imran98 <saad2379@gmail.com>
2021-03-21 21:28:39 +0000

    Created dialog for editing and adding menu categories

---
+++ RestaurantWebApp/src/app/category-dialog/category-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-dialog',
+  templateUrl: './category-dialog.component.html',
+  styleUrls: ['./category-dialog.component.sass']
+})
+export class CategoryDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
