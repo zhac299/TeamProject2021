6e610fd98f263795578fdd8ff03f47debb9909cb
Hassan Mirza <zhac299@live.rhul.ac.uk>
2021-03-23 22:20:46 +0000

    gets all tables from backend for drop down menu.

--- RestaurantWebApp/src/app/manager-menu/add-staff-dialog/add-staff-dialog.component.ts
+++ RestaurantWebApp/src/app/manager-menu/add-staff-dialog/add-staff-dialog.component.ts
@@ -1,19 +1,31 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { Staff } from '../../../models/Staff';
+import { Table } from '../../../models/Table';
+import { TableService } from '../../table.service';
 
 @Component({
   selector: 'app-add-staff-dialog',
   templateUrl: './add-staff-dialog.component.html',
   styleUrls: ['./add-staff-dialog.component.sass']
 })
+
 export class AddStaffDialogComponent implements OnInit {
 
   selected = -1;
+
   constructor(public dialogRef: MatDialogRef<AddStaffDialogComponent>,
+              public tableService: TableService,
               @Inject(MAT_DIALOG_DATA) public data: { staff:Staff,title:string }) { }
 
-  ngOnInit(): void {}
+  tableList: Table[] = [];
+  selectedTable: Table = null;
+
+  ngOnInit(): void {
+    this.tableService.getTables().subscribe(table => {
+      this.tableList = table;
+    })
+  }
 
   setCalories(value: number) {
     return value;
@@ -26,6 +38,12 @@ export class AddStaffDialogComponent implements OnInit {
 
   setData(staff:Staff) {
     this.data.staff = staff;
-  }
+  } 
 
+  setTable(): void {
+    if (this.selectedTable != null) {
+      this.tableService.assignTable(this.selectedTable);
+    } 
+    this.dialogRef.close();
+  }  
 }
