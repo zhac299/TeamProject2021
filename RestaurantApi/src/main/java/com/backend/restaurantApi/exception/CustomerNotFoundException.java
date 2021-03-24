64b695b95c767b1237a12329739f1bf08dfffd00
CalebBailey <77368091+CalebBailey@users.noreply.github.com>
2021-03-24 20:28:29 +0000

    Update CustomerNotFoundException.java

--- RestaurantApi/src/main/java/com/backend/restaurantApi/exception/CustomerNotFoundException.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/exception/CustomerNotFoundException.java
@@ -1,7 +1,15 @@
 package com.backend.restaurantApi.exception;
 
+/**
+ * The class handles the error that is thrown when a customer is not found.
+ */
 public class CustomerNotFoundException extends RuntimeException {
     
+    /**
+     * The error constructor.
+     * 
+     * @param message the error message
+     */
     public CustomerNotFoundException(String message) {
         super(message);
     }
