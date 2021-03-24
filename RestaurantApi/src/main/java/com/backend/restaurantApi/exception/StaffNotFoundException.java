e7c4798b63d8029bf1d28cb3280bbaceadd6d596
CalebBailey <77368091+CalebBailey@users.noreply.github.com>
2021-03-24 20:30:16 +0000

    Update StaffNotFoundException.java

--- RestaurantApi/src/main/java/com/backend/restaurantApi/exception/StaffNotFoundException.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/exception/StaffNotFoundException.java
@@ -1,7 +1,15 @@
 package com.backend.restaurantApi.exception;
 
+/**
+ * The class handles the error that is thrown when a staff member is not found.
+ */
 public class StaffNotFoundException extends RuntimeException {
     
+    /**
+     * The error constructor. 
+     * 
+     * @param message the error message
+     */
     public StaffNotFoundException(String message) {
         super(message);
     }
