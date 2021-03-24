b94d9836971e6debfe4e0c93091b18fab295ca99
Cosmin Sirbu <zhac254@live.rhul.ac.uk>
2021-03-24 22:51:20 +0200

    Added doc for the table service

--- RestaurantWebApp/src/app/table.service.ts
+++ RestaurantWebApp/src/app/table.service.ts
@@ -10,29 +10,64 @@ import { Staff } from '../models/Staff';
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * The class that handles requests for the Table DB.
+ */
 export class TableService {
 
+  /**
+   * The URL link that is used to make requests to the DB.
+   */
   private restaurantTablesURL = 'http://localhost:8080/api/v1/tables';
 
+  /**
+   * The current staff assigned to a table.
+   */
   public currentStaff: number;
 
+  /**
+   * Helper Subject to store orders for state management.
+   */
   private readonly tableSubject$ = new BehaviorSubject<Table[]>(new Array<Table>());
 
+  /**
+   * A subject that is called when a new request is needed.
+   */
   private _refreshNeeded$ = new Subject<void>();
 
+  /**
+   * Gets the private class subject refresh needed
+   */
   get refreshNeeded() {
     return this._refreshNeeded$;
   }
 
+  /**
+   * A subject that can be called if new observables need to be emitted.
+   */
   refresh$ = new BehaviorSubject(null);
 
+  /**
+   * A public observable that can be subscribed to by components.
+   */
   tables$ = this.refresh$.pipe(
     exhaustMap(() => this.getTables()),
     share()
   );
 
+  /**
+   * The constructor of the class.
+   * 
+   * @param httpClient the http client that is used to make requests to the DB
+   */
   constructor(private httpClient: HttpClient) { }
 
+  /**
+   * Gets all the tables by making a get request to the DB.
+   * 
+   * @returns an observable of type Table[]
+   */
   public getTables(): Observable<Table[]> {
     return this.httpClient.get<Table[]>(this.restaurantTablesURL)
       .pipe(
@@ -40,6 +75,9 @@ export class TableService {
       );
   }
 
+  /**
+   * Gets all updated tables (by the subject).
+   */
   public getUpdatedTables(): void {
     this.httpClient.get<Table[]>(this.restaurantTablesURL)
       .subscribe((tables) => {
@@ -47,6 +85,12 @@ export class TableService {
       });
   }
 
+  /**
+   * Gets a table by its number by making a get request to the DB.
+   * 
+   * @param id the id of the queried table
+   * @returns an observable of type Table
+   */
   public getTableByNumber(id: number): Observable<Table> {
     return this.httpClient.get<Table>(`${this.restaurantTablesURL}/${id}`)
       .pipe(
@@ -56,6 +100,11 @@ export class TableService {
       )
   }
 
+  /**
+   * Updates a table by making a put request to the DB.
+   * 
+   * @param table the table that needs to be updated
+   */
   public updateTable(table: Table): void {
     this.httpClient.put<Table>(`${this.restaurantTablesURL}/${table.tableNumber}`, table)
     .subscribe((table) => {
@@ -69,6 +118,11 @@ export class TableService {
     })
   }
 
+  /**
+   * Gets all unoccupied tables by making a get request to the DB.
+   * 
+   * @returns an observable of type Table[]
+   */
   public getUnoccupiedTables(): Observable<Table[]> {
     let restaurantTablesUnoccupiedURL: string = this.restaurantTablesURL + '/unoccupied'
     return this.httpClient.get<Table[]>(restaurantTablesUnoccupiedURL)
@@ -77,6 +131,11 @@ export class TableService {
       );
   }
 
+  /**
+   * Gets all tables that need help by making a get request to the DB.
+   * 
+   * @returns an observable of type Table[]
+   */
   public getNeedHelpTables(): Observable<Table[]> {
     let restaurantTablesNeedHelpURL: string = this.restaurantTablesURL + '/needHelp'
     return this.httpClient.get<Table[]>(restaurantTablesNeedHelpURL)
@@ -85,6 +144,12 @@ export class TableService {
       );
   }
 
+  /**
+   * Deletes a table from the DB by making a delete request to the DB.
+   * 
+   * @param table the table that needs to be deleted
+   * @returns an observable of type Table
+   */
   public deleteTable(table: Table): Observable<Table> {
     return this.httpClient.delete<Table>(`${this.restaurantTablesURL}/${table.tableNumber}`)
       .pipe(
@@ -94,6 +159,11 @@ export class TableService {
       );
   }
 
+  /**
+   * Creates a new table by making a post request to the DB.
+   * 
+   * @returns an observable of type Table
+   */
   public createTable(): Observable<Table> {
     const newTable = new Table;
     newTable.isOccupied = false;
@@ -103,6 +173,12 @@ export class TableService {
     return this.httpClient.post<Table>(this.restaurantTablesURL, newTable);
   }
 
+  /**
+   * Assigns the table to a waiter by making a post request to the DB.
+   * 
+   * @param table the table the waiter needs to be assigned to
+   * @returns an observable of type Table
+   */
   public assignTable(table: Table): Observable<Table> {
     return this.httpClient.post<Table>(this.restaurantTablesURL + '/assignTable', table);
   }
