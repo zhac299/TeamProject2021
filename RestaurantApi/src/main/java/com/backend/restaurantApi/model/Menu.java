a717bc0d3a0b7621ab538153a1687db62a4a10d0
lhamdou777 <lhamdouuni@gmail.com>
2021-03-25 15:14:52 +0000

    Finished javadoc for the Menu model.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/model/Menu.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Menu.java
@@ -273,7 +273,7 @@ public class Menu {
 
     /**
      * Setter for isCrustaceans.
-     * @param gluten boolean for if there is Crustaceans or not.
+     * @param crustaceans boolean for if there is Crustaceans or not.
      */
     public void setCrustaceans(boolean crustaceans) {
         this.crustaceans = crustaceans;
@@ -289,128 +289,248 @@ public class Menu {
 
     /**
      * Setter for eggs.
-     * @param gluten boolean for if there is eggs or not.
+     * @param eggs boolean for if there is eggs or not.
      */
     public void setEggs(boolean eggs) {
         this.eggs = eggs;
     }
 
+    /**
+     * Checks for fish.
+     * @return boolean according to if there are fish or not.
+     */
     public boolean isFish() {
         return fish;
     }
 
+    /**
+     * Setter for fish.
+     * @param fish boolean for if there is fish or not.
+     */
     public void setFish(boolean fish) {
         this.fish = fish;
     }
 
+    /**
+     * Checks for lupin.
+     * @return boolean according to if there are lupin or not.
+     */
     public boolean isLupin() {
         return lupin;
     }
 
+    /**
+     * Setter for lupin.
+     * @param lupin boolean for if there is fish or not.
+     */
     public void setLupin(boolean lupin) {
         this.lupin = lupin;
     }
 
+    /**
+     * Checks for milk.
+     * @return boolean according to if there are milk or not.
+     */
     public boolean isMilk() {
         return milk;
     }
 
+    /**
+     * Setter for milk.
+     * @param milk boolean for if there is milk or not.
+     */
     public void setMilk(boolean milk) {
         this.milk = milk;
     }
 
+    /**
+     * Checks for molluscs.
+     * @return boolean according to if there are molluscs or not.
+     */
     public boolean isMolluscs() {
         return molluscs;
     }
 
+    /**
+     * Setter for molluscs.
+     * @param molluscs boolean for if there is molluscs or not.
+     */
     public void setMolluscs(boolean molluscs) {
         this.molluscs = molluscs;
     }
 
+    /**
+     * Checks for mustard.
+     * @return boolean according to if there are mustard or not.
+     */
     public boolean isMustard() {
         return mustard;
     }
 
+    /**
+     * Setter for mustard.
+     * @param mustard boolean for if there is mustard or not.
+     */
     public void setMustard(boolean mustard) {
         this.mustard = mustard;
     }
 
+    /**
+     * Checks for nuts.
+     * @return boolean according to if there are nuts or not.
+     */
     public boolean isNuts() {
         return nuts;
     }
 
+    /**
+     * Setter for nuts.
+     * @param nuts boolean for if there is nuts or not.
+     */
     public void setNuts(boolean nuts) {
         this.nuts = nuts;
     }
 
+    /**
+     * Checks for soya.
+     * @return boolean according to if there are soya or not.
+     */
     public boolean isSoya() {
         return soya;
     }
 
+    /**
+     * Setter for soya.
+     * @param soya boolean for if there is soya or not.
+     */
     public void setSoya(boolean soya) {
         this.soya = soya;
     }
 
+    /**
+     * Checks for sesame.
+     * @return boolean according to if there are sesame or not.
+     */
     public boolean isSesameSeeds() {
         return sesameSeeds;
     }
 
+    /**
+     * Setter for sesame.
+     * @param sesame boolean for if there is sesame or not.
+     */
     public void setSesameSeeds(boolean sesameSeeds) {
         this.sesameSeeds = sesameSeeds;
     }
 
+    /**
+     * Checks for sulphites.
+     * @return boolean according to if there are sulphites or not.
+     */
     public boolean isSulphites() {
         return sulphites;
     }
 
+    /**
+     * Setter for sulphites.
+     * @param sulphites boolean for if there is sulphites or not.
+     */
     public void setSulphites(boolean sulphites) {
         this.sulphites = sulphites;
     }
 
+    /**
+     * Gets desctription of menu.
+     * @return String according to the description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for description.
+     * @param description String for intended description.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-
+    /**
+     * Gets the time to cook.
+     * @return double representing how long it takes to cook.
+     */
     public double getTimeToTime() {
         return this.timeToCook;
     }
+
+    /**
+     * Setter for suggestd.
+     * @param suggest String for intended suggestion.
+     */
     public void setSuggested(String suggest) {
         this.isSuggested = suggest;
     }
     
+    /**
+     * Getting the suggested item.
+     * @return suggested item as a String.
+     */
     public String getSuggested() {
         return isSuggested;
     }
 
+    /**
+     * Setter for id.
+     * @param id value for id.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Getter for the category.
+     * @return MenuCategory object.
+     */
     public MenuCategory getCategory() {
         return category;
     }
 
+    /**
+     * Setter for category.
+     * @param category intended category.
+     */
     public void setCategory(MenuCategory category) {
         this.category = category;
     }
 
+    /**
+     * Getter for time to cook.
+     * @return time to cook.
+     */
     public double getTimeToCook() {
         return timeToCook;
     }
 
+    /**
+     * Setter for timeToCook
+     * @param timeToCook amount of time to cook.
+     */
     public void setTimeToCook(double timeToCook) {
         this.timeToCook = timeToCook;
     }
 
+    /**
+     * Getter for picture url.
+     * @return url of picture.
+     */
     public String getPictureUrl() {
         return pictureUrl;
     }
 
+    /**
+     * Setter for picture url.
+     * @param pictureUrl picture url.
+     */
     public void setPictureUrl(String pictureUrl) {
         this.pictureUrl = pictureUrl;
     }
