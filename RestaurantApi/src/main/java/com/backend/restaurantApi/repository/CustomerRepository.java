e9652574b6db421b6d84c16ccb3a75deff62f3ef
lhamdou777 <lhamdouuni@gmail.com>
2021-03-25 15:39:50 +0000

    Javadoc for CustomerRepo.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/repository/CustomerRepository.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/repository/CustomerRepository.java
@@ -6,6 +6,8 @@ import com.backend.restaurantApi.model.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Repository class for the Customer.
+ */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {}
