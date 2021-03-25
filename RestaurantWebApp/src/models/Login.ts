4c7234cbb27308f1521267878fa3cd115b28f8aa
Cosmin Sirbu <zhac254@live.rhul.ac.uk>
2021-03-25 15:01:47 +0200

    Added doc to the models

--- RestaurantWebApp/src/models/Login.ts
+++ RestaurantWebApp/src/models/Login.ts
@@ -2,11 +2,39 @@
  * Login object with id, username, password, email and waiter boolean
  */
 export class Login {
+
+    /**
+     * The id of the login session.
+     */
     id: number;
+
+    /**
+     * The username of the staff.
+     */
     userName: string;
+
+    /**
+     * The password of the staff.
+     */
     password: string;
+
+    /**
+     * The email of the staff.
+     */
     email: string;
+
+    /**
+     * Asserts if the staff is waiter.
+     */
     waiter: boolean;
+
+    /**
+     * Asserts if the staff is a manager.
+     */
     manager: boolean;
+
+    /**
+     * The otp of the login.
+     */
     otp!: number;
   }
