1c2eba2a3042950b5deac4679ae23be0b4fe81f4
saad-imran98 <saad2379@gmail.com>
2021-03-25 12:52:55 +0000

    Added missing AngularDoc

--- RestaurantWebApp/src/app/manager-menu/add-staff/sales-dialog/sales-dialog.component.ts
+++ RestaurantWebApp/src/app/manager-menu/add-staff/sales-dialog/sales-dialog.component.ts
@@ -2,6 +2,9 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Staff} from "../../../../models/Staff";
 
+/**
+ * Dialog that shows sales information for a staff member
+ */
 @Component({
   selector: 'app-sales-dialog',
   templateUrl: './sales-dialog.component.html',
@@ -9,6 +12,11 @@ import {Staff} from "../../../../models/Staff";
 })
 export class SalesDialogComponent implements OnInit {
 
+  /**
+   *
+   * @param dialogRef to close dialog
+   * @param staff data injected from parent component
+   */
   constructor(public dialogRef: MatDialogRef<SalesDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: Staff
               ) { }
