445b50b02f38441e608ba5f1e5a0b495e085097c
lhamdou777 <lhamdouuni@gmail.com>
2021-03-25 16:07:25 +0000

    Javadoc done for Staff.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/model/Staff.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/model/Staff.java
@@ -46,6 +46,15 @@ public class Staff {
 	public Staff() {
 	}
 
+	/**
+	 * Staff constructor to make a staff object.
+	 * @param id of staff object.
+	 * @param userName of staff object.
+	 * @param password of staff object.
+	 * @param email of staff object.
+	 * @param isWaiter of staff object.
+	 * @param isManager of staff object.
+	 */
 	public Staff(long id, String userName, String password, String email, boolean isWaiter, boolean isManager) {
 		this.id = id;
 		this.userName = userName;
@@ -55,50 +64,98 @@ public class Staff {
 		this.isManager = isManager;
 	}
 
+	/**
+	 * Getter for id.
+	 * @return id.
+	 */
 	public long getId() {
 		return id;
 	}
 
+	/**
+	 * Setter for id.
+	 * @param id to set to.
+	 */
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getter for username.
+	 * @return the username.
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * Setter for the username.
+	 * @param userName to set to.
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	/**
+	 * Getter for the password.
+	 * @return the password.
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * Setter for password.
+	 * @param password to set to.
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * Getter for email.
+	 * @return email in String format.
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * Setter for the email.
+	 * @param email to set to.
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * Checks if staff is waiter.
+	 * @return boolean representation.
+	 */
 	public boolean isWaiter() {
 		return isWaiter;
 	}
 
+	/**
+	 * Setter for isWaiter.
+	 * @param isWaiter to set to.
+	 */
 	public void setWaiter(boolean isWaiter) {
 		this.isWaiter = isWaiter;
 	}
 
+	/**
+	 * Checks if staff is manager.
+	 * @return boolean representation of it the staff is waiter or not.
+	 */
 	public boolean isManager() {
 		return this.isManager;
 	}
 
+	/**
+	 * Setter for the is Manager.
+	 * @param isManager to set to.
+	 */
 	public void setIsManager(boolean isManager) {
 		this.isManager = isManager;
 	}
