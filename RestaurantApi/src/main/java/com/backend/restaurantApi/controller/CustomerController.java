d5d16e12a9acbacb208dfeeb4616046fef1d6104
Hassan Mirza <zhac299@live.rhul.ac.uk>
2021-03-25 17:48:48 +0000

    Adding documentation to class delcaration of Order Controller.

--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/CustomerController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/CustomerController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 /**
  * Controller of the Customer.
  */
--- RestaurantApi/src/main/java/com/backend/restaurantApi/controller/OrderController.java
+++ RestaurantApi/src/main/java/com/backend/restaurantApi/controller/OrderController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.PriorityQueue;
 
+/**
+ * Controller of the Order.
+ */
 @CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/api/v1/")
